6f41b093d818606fc88edc59819c4357b2350f34
Yan Scherbatov <yscherbatov@ptsecurity.com>
2022-11-12 14:41:24 +0700

    Add main.py

---
+++ main.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass(frozen=True)
+class Student:
+    first_name: str
+    last_name: str
+    age: int
+
+    @property
+    def name(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+
+class Group:
+    def __init__(self, group_number):
+        self._students = []
+        self._group_number = group_number
+    
+    def add_student(self, student: Student):
+        self._students.append(student)
+    
+    def add_students(self, students: List[Student]):
+        for student in students:
+            self.add_student(student)
+    
+    def print_info(self):
+        print(f'Когорта номер {self._group_number}')
+        print(f'В когорте {len(self._students)} студента:')
+        for student in self._students:
+            print(student.name)
+
+
+if __name__ == '__main__':
+    group = Group(45)
+    group.add_students(
+        [
+            Student(
+                first_name='Иван',
+                last_name='Демидов',
+                age=59,
+            ),
+            Student(
+                first_name='Виктория',
+                last_name='Сикрет',
+                age=18,
+            ),
+            Student(
+                first_name='Тимоти',
+                last_name='Шаламе',
+                age=26,
+            ),
+        ]
+    )
+    group.print_info()
